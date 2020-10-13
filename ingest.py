998acef0b6dcbd34f8f5c9dca5e06f0118fcd15f
Keyser Soze <christopherjgaines@gmail.com>
2020-10-13 00:00:33 -0400

    rearrange code to prep for pub/sub

--- ingest.py
+++ ingest.py
@@ -1,10 +1,27 @@
 import requests
+import signal
 import json
 
 def ingest(period=60):
+	initlist = []
 	r = requests.get('http://stream.meetup.com/2/rsvps', stream=True)
 	for line in r.iter_lines():
 		decoded_line = line.decode('utf-8')
-		print(json.loads(decoded_line))
+		jsondict = json.loads(decoded_line)
+		print(jsondict)
+		# publish(jsondict)
 
-ingest()
+class TimeoutException(Exception):
+	pass
+
+def timeout_handler(signum, frame):
+	raise TimeoutException
+
+signal.signal(signal.SIGALRM, timeout_handler)
+signal.alarm(60)    
+try:
+	ingest()
+except TimeoutException:
+	sdfghj
+else:
+	signal.alarm(0)
