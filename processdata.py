ff41549cdc58c2b9961bfd14917ad5024624c0f8
Keyser Soze <christopherjgaines@gmail.com>
2020-10-13 00:46:46 -0400

    collect relevant info at processing

--- processdata.py
+++ processdata.py
@@ -1,4 +1,27 @@
 import signal
 
 def processdata(event):
-	print(event)
+	eventTime = event['event']['time']
+	eventUrl = event['event']['event_url']
+	eventCountry = event['group']['group_country']
+	return eventTime, eventUrl, eventCountry
+
+
+# each event we need the following info
+
+## event['event']['time']
+
+## event['event']['event_url']
+
+## event['group']['group_country']
+
+# count the following
+
+## total number of rsvps in the time period
+
+## the count of each group country during the time period
+
+# the data should be in the following format
+
+## total,future_date,future_url,co_1,co_1_count,co_2,co_2_count,co_3,co_3_count
+
