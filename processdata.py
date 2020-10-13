8f959e74d746d8e5b201ac1078679724a4fb80df
Keyser Soze <christopherjgaines@gmail.com>
2020-10-13 03:32:59 -0400

    add comments for readability

--- processdata.py
+++ processdata.py
@@ -2,16 +2,20 @@ import operator
 import itertools
 
 def processData(event, total, eventTime, eventUrl, countries_dict):
-	if event['event']['time'] > eventTime:
+	if event['event']['time'] > eventTime: # update eventTime and eventUrl if new event (event['event']['time']) is at a later date
 		eventTime = event['event']['time']
 		eventUrl = event['event']['event_url']
 	eventCountry = event['group']['group_country']
-	if eventCountry in countries_dict:
+
+	if eventCountry in countries_dict: # add country to dictionary if not there and initialize at 1
 		countries_dict[eventCountry] += 1
-	else:
+	else: # if country is here add 1 to the number of events with this country code
 		countries_dict[eventCountry] = 1
+
+	# sort country dict to highest three countries code counts
 	sorted_d = sorted(countries_dict.items(), key=operator.itemgetter(1),reverse=True)[:3]
 	flatten = list(itertools.chain(*sorted_d))
+
 	if len(flatten) == 2:
 		co_1, co_1_count = flatten
 		return total, eventTime, eventUrl, co_1, co_1_count
