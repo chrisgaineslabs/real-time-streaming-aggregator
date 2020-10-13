9f8094b575f1bb6b31fb60e091402ee45d0364b4
Keyser Soze <christopherjgaines@gmail.com>
2020-10-13 00:35:08 -0400

    rearrange code

--- ingest.py
+++ ingest.py
@@ -1,24 +1,26 @@
+from processdata import processdata
 import requests
 import signal
 import json
 
-def ingest(period=60):
+time_to_run_program = 60 # in secs
+
+def ingest():
 	initlist = []
 	r = requests.get('http://stream.meetup.com/2/rsvps', stream=True)
 	for line in r.iter_lines():
 		decoded_line = line.decode('utf-8')
 		jsondict = json.loads(decoded_line)
-		print(jsondict)
-		# publish(jsondict)
+		processdata(jsondict)
 
-class TimeoutException(Exception):
+class TimeoutException(Exception): #code from https://stackoverflow.com/questions/25027122/break-the-function-after-certain-time
 	pass
 
 def timeout_handler(signum, frame):
 	raise TimeoutException
 
 signal.signal(signal.SIGALRM, timeout_handler)
-signal.alarm(60)    
+signal.alarm(time_to_run_program)    
 try:
 	ingest()
 except TimeoutException:
